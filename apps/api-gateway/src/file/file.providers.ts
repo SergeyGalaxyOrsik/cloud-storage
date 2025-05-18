198553530422fa0770b5b7514d3698239a02532e
SergeyGalaxyOrsik <93079311+SergeyGalaxyOrsik@users.noreply.github.com>
2025-05-18 21:58:32 +0300

    a

--- apps/api-gateway/src/file/file.providers.ts
+++ apps/api-gateway/src/file/file.providers.ts
@@ -1,9 +1,13 @@
-import { Client } from '@temporalio/client';
+import { Client, Connection } from '@temporalio/client';
 import { Provider } from '@nestjs/common';
 
 export const TemporalClientProvider: Provider = {
   provide: Client,
   useFactory: async () => {
-    return new Client(); // можно передать options
+    return new Client({
+      connection: await Connection.connect({
+        address: process.env.TEMPORAL_ADDRESS || 'temporal:7233'
+      })
+    }); // можно передать options
   },
 };
